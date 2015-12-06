74ce9d01d360a9cdabdbbbb024085789bc903d6e
tomaszgawlik <tomasz.gawlik@hotmail.com>
2015-12-06 00:20:37 +0000

    Render both map and search div

--- MoviesMap.Web/public/js/app.js
+++ MoviesMap.Web/public/js/app.js
@@ -1,5 +1,12 @@
 angular.module('moviesMapApp', ['uiGmapgoogle-maps'])
   .controller('MapController', function($scope) {
-    
     $scope.map = { center: { latitude: 45, longitude: -73 }, zoom: 8 };
+  })
+  .controller('SearchController', function($scope){
+    
+    $scope.searchQuery = '';
+    
+    $scope.searchForMovies = function() {
+      console.log("Search for movies with query: " + $scope.searchQuery);
+    }
   });
