c21560ca0b7ca0a28d52ae50f324e59a8bd9066f
tomaszgawlik <tomasz.gawlik@hotmail.com>
2015-12-06 15:11:10 +0000

    Add gulp and nodemon task

---
+++ MoviesMap.Web/gulpfile.js
@@ -0,0 +1,10 @@
+var gulp = require('gulp');
+var nodemon = require('nodemon');
+
+gulp.task('start', function () {
+  nodemon({
+    script: 'server.js'
+  , ext: 'js html'
+  , env: { 'NODE_ENV': 'development' }
+  })
+})
