c8f0e79d5ee48aae2de641fc41b53e1acdebb48e
tomaszgawlik <tomasz.gawlik@hotmail.com>
2016-01-10 23:57:38 +0000

    Add all layers and events for map center being changed

--- MoviesMap.Web/public/js/app/mapController.js
+++ MoviesMap.Web/public/js/app/mapController.js
@@ -1,25 +1,42 @@
 angular.module('moviesMapApp')
-.controller('MapController', function($scope) {
-    $scope.map = { 
+.controller('MapController', function($scope, $http) {
+    $scope.map =  { 
       center: { 
         latitude: 51.5285582,
-        longitude: -0.2416805 }, 
-        zoom: 11 };
+        longitude: -0.2416805 
+      }, 
+      zoom: 11,
+      events: {
+            dragend: function (map) {
+                $scope.updateMoviesOnMap(map.getCenter().lat(), map.getCenter().lng());
+            }
+      } 
+    };
     
     $scope.markers = {
       nearby: [],
       icon: "icon",
       coords: "self"
     }
-       
-    var marker = {
-        latitude: 51.5285582,
-        longitude: -0.2416805,
-        title: 'London',
-        id: "id" // TODO: default somewhere 
+    
+    $scope.updateMoviesOnMap = function(latitude, longitude){
+      var request = {
+            latitude: latitude,
+            longitude: longitude
       };
       
-    // TODO: ajax request, get actuall points based on current location
-    $scope.markers.nearby = [marker];
+      console.log("Request: "+JSON.stringify(request));
+
+      $http.post('/movies/nearby', request)
+           .then(function(response){
+               console.log(response.data);
+             $scope.markers.nearby = response.data;
+           }, function(errorResponse){
+              $scope.markers.nearby = [];
+           });
+    }
     
+    // initial load
+    $scope.updateMoviesOnMap($scope.map.center.latitude, $scope.map.center.longitude);
+   
   });
