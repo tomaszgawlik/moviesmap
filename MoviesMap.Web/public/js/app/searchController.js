96a070ee19836148eb04450b418f0b3039c1a66d
TK <kutylatom@gmail.com>
2016-02-08 23:38:51 +0100

    Usunięcie searchControllera.

--- MoviesMap.Web/public/js/app/searchController.js
+++
@@ -1,9 +0,0 @@
-angular.module('moviesMapApp')
-.controller('SearchController', function($scope){
-    
-    $scope.searchQuery = '';
-    
-    $scope.searchForMovies = function() {
-      console.log("Search for movies with query: " + $scope.searchQuery);
-    }
-  });
